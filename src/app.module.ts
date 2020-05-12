5e2b23baba67e07e84730e91b779d89f5456faf5
Hossam Aldokkani <hossam.aldokkani@gmail.com>
2020-05-13 01:23:38 +0200

    Format code with tabWidth=4

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,29 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationModule } from './notification/notification.module';
 
 const {
-  MONGO_HOST,
-  MONGO_PORT,
-  MONGO_DATABASE,
-  MONGO_USER,
-  MONGO_PASSWORD,
+    MONGO_HOST,
+    MONGO_PORT,
+    MONGO_DATABASE,
+    MONGO_USER,
+    MONGO_PASSWORD,
 } = process.env;
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      },
-    ),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(
+            `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+            },
+        ),
+        NotificationModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
